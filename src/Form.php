e7e97e87a66f4ca546a1f885012bc3d36fa3d589
Rudolph Gottesheim <r.gottesheim@loot.at>
2014-06-24 11:50:51 +0200

    Add Midnight\FormModule\Form to provide an addClass() method

---
+++ src/Form.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Midnight\FormModule;
+
+use Zend\Form\Form as ZendForm;
+
+class Form extends ZendForm
+{
+    /**
+     * @param string $class
+     */
+    public function addClass($class)
+    {
+        $classes = explode(' ', $this->getAttribute('class'));
+        $classes[] = $class;
+        $classes = $this->cleanupClasses($classes);
+        $this->setAttribute('class', join(' ', $classes));
+    }
+
+    /**
+     * Removes unnecessary spaces and duplicates
+     *
+     * @param string[] $classes
+     *
+     * @return string[]
+     */
+    private function cleanupClasses($classes)
+    {
+        foreach ($classes as $index => &$class) {
+            $class = trim($class);
+            if (empty($class)) {
+                unset($classes[$index]);
+            }
+        }
+        $classes = array_unique($classes);
+        return $classes;
+    }
+} 
